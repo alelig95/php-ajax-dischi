d81d3b92a58d90500c688e90cd2b923d3c6457e4
Alessandro Liggieri <smmb@live.it>
2021-03-02 14:47:54 +0100

    Creata la struttura delle cartelle

---
+++ database/database.php
@@ -0,0 +1,47 @@
+<?php
+$dischi = [
+  [
+    "poster" => "",
+    "title" => "",
+    "author" => "",
+    "genre" => "",
+    "year" => ""
+  ],
+  [
+    "poster" => "",
+    "title" => "",
+    "author" => "",
+    "genre" => "",
+    "year" => ""
+  ],
+  [
+    "poster" => "",
+    "title" => "",
+    "author" => "",
+    "genre" => "",
+    "year" => ""
+  ],
+  [
+    "poster" => "",
+    "title" => "",
+    "author" => "",
+    "genre" => "",
+    "year" => ""
+  ],
+  [
+    "poster" => "",
+    "title" => "",
+    "author" => "",
+    "genre" => "",
+    "year" => ""
+  ],
+  [
+    "poster" => "",
+    "title" => "",
+    "author" => "",
+    "genre" => "",
+    "year" => ""
+  ]
+];
+echo "ciao";
+ ?>
