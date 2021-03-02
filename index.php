16ae367cf7fe68169a1402f6c0f702727dabae83
Alessandro Liggieri <smmb@live.it>
2021-03-02 18:03:23 +0100

    Milestone 1 completa

--- index.php
+++ index.php
@@ -13,32 +13,30 @@ require_once __DIR__ . '/database/database.php';
   </head>
   <body>
 
-
     <header>
       <div class="container">
         <img class="logo" src="https://1000logos.net/wp-content/uploads/2017/08/Spotify-Logo.png" alt="logo" />
       </div>
     </header>
 
-    <?php
-
-      foreach ($dischi as $key => $value) {
-
-    ?>
-
     <div class="cds-container container">
+      <?php
+
+        foreach ($dischi as $key => $value) {
+
+      ?>
+
       <div class="cd">
         <img src="<?php echo $value['poster']?>" alt="">
         <h3><?php echo $value["title"] ?></h3>
         <span class="author"><?php echo $value["author"] ?></span>
         <span class="year"><?php echo $value["year"] ?></span>
       </div>
+
+      <?php
+       }
+      ?>
     </div>
-
-     <?php
-      }
-     ?>
-
 
   </body>
 </html>
